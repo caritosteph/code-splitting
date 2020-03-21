0e25868e989a2121f536371aad1e143c4b20b8c4
Carolyn Ulfe <carolyn.ulfe@gmail.com>
2020-03-21 17:02:11 +0100

    feat: Creating basic components
    Rendering pages components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles.css";
+import Page1 from "./Components/Page1";
+import Page2 from "./Components/Page2";
+import Page3 from "./Components/Page3";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Page1 />
+      <Page2 />
+      <Page3 />
     </div>
   );
 }
